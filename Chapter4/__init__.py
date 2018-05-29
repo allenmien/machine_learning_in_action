a5db9cbcf53d8c678f2da859f59e11b24c97c98d
mark.ma <mark.ma@qingbao.cn>
2018-05-29 19:59:27 +0800

    仓库初始化

---
+++ Chapter4/__init__.py
@@ -0,0 +1,6 @@
+# -*-coding:utf-8-*-
+"""
+@author:Mark
+@file: __init__.py.py 
+@time: 2018/04/21
+"""
---
+++ run.py
@@ -0,0 +1,10 @@
+# -*-coding:utf-8-*-
+"""
+@author:Mark
+@file: run.py 
+@time: 2018/04/21
+"""
+from Chapter4 import Bayes
+
+if __name__ == u"__main__":
+    Bayes.testingNB()
