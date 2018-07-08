0997ef455f6a3274cbbb5194bc949d2243fcc702
mark.ma <mark.ma@qingbao.cn>
2018-07-08 19:18:53 +0800

    贝叶斯分类代码上传

---
+++ Bayes/__init__.py
@@ -0,0 +1,6 @@
+# -*-coding:utf-8-*-
+"""
+@Time   : 2018/6/15 15:08
+@Author : Mark
+@File   : __init__.py.py
+"""
--- run.py
+++ run.py
@@ -4,7 +4,11 @@
 @file: run.py 
 @time: 2018/04/21
 """
-import KMeans.kNN as KNN
+import Bayes.bayes as Bayes
 
 if __name__ == u"__main__":
-    KNN.handwritingClassTest()
+    # postingList, classVec = Bayes.loadDataSet()
+    # vocabList = Bayes.createVocabList(postingList)
+    # WordsVec = Bayes.setOfWords2Vec(vocabList, postingList[0])
+    Bayes.testingNB()
+    pass
